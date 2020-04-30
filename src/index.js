1537e402257e3116c79b396a80105de13ae72f20
Marcos Siqueira <MarcosS@datacom.co.nz>
2020-05-01 08:17:14 +1200

    initial setup for drag and drop react

---
+++ src/index.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './column'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`
+
+class App extends React.Component {
+  state = initialData;
+  onDragStart = start => {
+    const homeIndex = this.state.columnOrder.indexOf(start.source.droppableId);
+    this.setState({
+      homeIndex,
+    });
+  };
+
+  onDragEnd = result => {
+    this.setState({
+      homeIndex: null
+    });
+
+    document.body.style.color = 'inherit';
+    document.body.style.transition = 'inherit';
+
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (destination.droppableId === source.droppableId &&
+      destination.index === source.index) {
+      return;
+    }
+
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds)
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn
+        }
+      };
+
+      this.setState(newState);
+      return;
+    }
+
+    // Moving from one list to another
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+    this.setState(newState);
+
+  };
+
+  render() {
+    return (
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragEnd={this.onDragEnd}>
+        <Container>
+          {this.state.columnOrder.map((columnId, index) => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+            const isDropDisabled = index < this.state.homeIndex;
+            return <Column key={column.id} column={column} tasks={tasks} isDropDisabled={isDropDisabled} />;
+          })}
+        </Container>
+      </DragDropContext>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
+
---
+++ src/task.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: ${props => (props.isDragDisabled ? 'lightgray' : props.isDragging ? 'lightgreen' : 'white')};
+`;
+
+export default class Task extends React.Component {
+  render() {
+    const isDragDisabled = this.props.task.id === 'task-1';
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index} isDragDisabled={isDragDisabled}>
+        {(provided, snapshot) => (
+          <Container
+            {...provided.dragHandleProps}
+            {...provided.draggableProps}
+            ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
+            isDragDisabled={isDragDisabled}
+          >{this.props.task.content}</Container>
+        )}
+      </Draggable>
+    );
+  }
+}
